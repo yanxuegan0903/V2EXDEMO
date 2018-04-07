02e171f61111b1b9210fc72c3264ca8936af4bb8
[username] <yanxuegan0903@163.com>
2018-04-07 14:14:13 +0800

    Initial Commit

---
+++ V2EX_DemoTests/V2EX_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  V2EX_DemoTests.swift
+//  V2EX_DemoTests
+//
+//  Created by vsKing on 2018/4/7.
+//  Copyright © 2018年 vsKing. All rights reserved.
+//
+
+import XCTest
+@testable import V2EX_Demo
+
+class V2EX_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
